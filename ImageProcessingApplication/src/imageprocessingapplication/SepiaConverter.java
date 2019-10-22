d58a3ea317d50ecfc83b73c287182be70b24b378
Akhil <akhilramkee123@gmail.com>
2019-10-22 09:28:35 +0530

    Added a Basic format for the image processing library

---
+++ ImageProcessingApplication/src/imageprocessingapplication/SepiaConverter.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imageprocessingapplication;
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+/**
+ *
+ * @author Akhilesh
+ */
+public class SepiaConverter {
+    
+    BufferedImage image;
+    
+    public SepiaConverter(){
+        this.image = null;
+    }
+    
+    public SepiaConverter(BufferedImage image){
+        this.image = image;
+    }
+    
+    public BufferedImage convertToSepia(BufferedImage img){
+        this.image = img;
+        int height = image.getHeight();
+        int width = image.getWidth();
+        
+        for(int y=0;y < height;y++){
+            for(int x=0; x<width;x++){
+                int pixel = img.getRGB(x,y);
+                
+                int a = (pixel>>24)&0xff;
+                int R = (pixel>>16)&0xff;
+                int G = (pixel>>8)&0xff;
+                int B = pixel&0xff;
+                
+                int newRed = (int)(0.393*R+0.769*G+0.189*B);
+                int newGreen = (int)(0.349*R+0.686*G+0.168*B);
+                int newBlue = (int)(0.272*R+0.534*G+0.131*B);
+                
+                if(newRed>255)
+                    R=255;
+                else
+                    R=newRed;
+                if(newGreen>255)
+                    G = 255;
+                else
+                    G =newGreen;
+                if(newBlue>255)
+                    B=255;
+                else
+                    B=newBlue;
+                
+                pixel = (a<<24)|(R<<16)|(G<<8)|B;
+                
+                image.setRGB(x, y, pixel);
+            }
+        }
+        
+        return image;
+    }
+    
+}
